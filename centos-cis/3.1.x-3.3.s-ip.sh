7d1f6aa0a1a0bc180d382c01d640ad2e814c7786
shawngmc <shawngmc@gmail.com>
2019-06-13 18:48:19 -0400

    Create 3.1.x-3.3.s-ip.sh

---
+++ centos-cis/3.1.x-3.3.s-ip.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "3.1.x/3.2.1 IPv4 config..."
+cat > /etc/sysctl.conf << 'EOF'
+net.ipv4.ip_forward=0
+net.ipv4.conf.all.send_redirects=0
+net.ipv4.conf.default.send_redirects=0
+net.ipv4.conf.all.accept_source_route=0
+net.ipv4.conf.default.accept_source_route=0
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default.accept_redirects=0
+net.ipv4.conf.all.secure_redirects=0
+net.ipv4.conf.default.secure_redirects=0
+net.ipv4.conf.all.log_martians=1
+net.ipv4.conf.default.log_martians=1
+net.ipv4.route.flush=1
+net.ipv4.icmp_echo_ignore_broadcasts=1
+net.ipv4.icmp_ignore_bogus_error_responses=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.tcp_syncookies=1
+net.ipv6.conf.all.accept_ra=0
+net.ipv6.conf.default.accept_ra=0
+net.ipv6.conf.all.accept_redirects=0
+net.ipv6.conf.default.accept_redirects=0
+EOF
+ 
+echo "3.3.x Disabling IPv6..."
+cp /etc/sysconfig/network $AUDITDIR/network_$TIME.bak
+echo "NETWORKING_IPV6=no" >> /etc/sysconfig/network
+echo "IPV6INIT=no" >> /etc/sysconfig/network
+echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.d/ipv6.conf
