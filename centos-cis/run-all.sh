3212adf6531fa6b7ddf2bbedf0ac5e5f63566a63
shawngmc <shawngmc@gmail.com>
2019-06-13 18:59:26 -0400

    Create run-all.sh

---
+++ centos-cis/run-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./server_prep/centos-cis/1.1.1-uncommon-filesystems.sh
+./server_prep/centos-cis/2.2.1-nettime.sh
+./server_prep/centos-cis/3.1.x-3.3.s-ip.sh
+./server_prep/centos-cis/5.1.x-crontab.sh
+./server_prep/centos-cis/5.2.x-ssh.sh
+./server_prep/centos-cis/5.4.1-password.sh
