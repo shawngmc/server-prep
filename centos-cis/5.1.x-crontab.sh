efb0e47635dd7fec84a9349a19676567e3ad7a96
shawngmc <shawngmc@gmail.com>
2019-06-13 18:43:57 -0400

    Create 5.1.x-crontab.sh

---
+++ centos-cis/5.1.x-crontab.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#5.1.2-5.1.8 Cron Permissions
+chown root:root /etc/crontab
+chmod og-rwx /etc/crontab
+chown root:root /etc/cron.hourly
+chmod og-rwx /etc/cron.hourly
+chown root:root /etc/cron.daily
+chmod og-rwx /etc/cron.daily
+chown root:root /etc/cron.weekly
+chmod og-rwx /etc/cron.weekly
+chown root:root /etc/cron.monthly
+chmod og-rwx /etc/cron.monthly
+chown root:root /etc/cron.d
+chmod og-rwx /etc/cron.d
+rm -f /etc/cron.deny
+rm -f /etc/at.deny
+touch /etc/cron.allow
+touch /etc/at.allow
+chmod og-rwx /etc/cron.allow
+chmod og-rwx /etc/at.allow
+chown root:root /etc/cron.allow
+chown root:root /etc/at.allow
