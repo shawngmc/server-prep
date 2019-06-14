f7c37ffecd2f95f8c71a0715782ed461fdaae03f
Shawn McNaughton <shawngmc@gmail.com>
2019-06-14 05:57:34 +0000

    fixes

--- centos-cis/pull-cis-scanner.sh
+++ centos-cis/pull-cis-scanner.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -LO https://raw.githubusercontent.com/finalduty/cis_benchmarks_audit/master/cis-audit.sh
-chmod 750 cis-audit.sh
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+curl -L -o $scriptDir/cis-audit.sh https://raw.githubusercontent.com/finalduty/cis_benchmarks_audit/master/cis-audit.sh
+chmod 755 $scriptDir/cis-audit.sh
--- centos-cis/run-all.sh
+++ centos-cis/run-all.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
 mkdir -pv /var/log/server-prep/
 
 # Get updates 
 yum check-update 2>&1 | tee /var/log/server-prep/updates.log
 
 # Run customized CIS mitigations
-./1.1.1-uncommon-filesystems.sh 2>&1 | tee /var/log/server-prep/cis.log
-./2.2.1-nettime.sh 2>&1 | tee /var/log/server-prep/cis.log
-./3.1.x-3.3.s-ip.sh 2>&1 | tee /var/log/server-prep/cis.log
-./5.1.x-crontab.sh 2>&1 | tee /var/log/server-prep/cis.log
-./5.2.x-ssh.sh 2>&1 | tee /var/log/server-prep/cis.log
-./5.4.1-password.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/1.1.1-uncommon-filesystems.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/2.2.1-nettime.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/3.1.x-3.3.s-ip.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/5.1.x-crontab.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/5.2.x-ssh.sh 2>&1 | tee /var/log/server-prep/cis.log
+$scriptDir/5.4.1-password.sh 2>&1 | tee /var/log/server-prep/cis.log
 
 # Pull scanner and run
-./pull-cis-scanner.sh | tee /var/log/server-prep/centos-audit.log
-./cis-audit.sh 2>&1 | tee /var/log/server-prep/centos-audit.log
+$scriptDir/pull-cis-scanner.sh | tee /var/log/server-prep/centos-audit.log
+$scriptDir/cis-audit.sh 2>&1 | tee /var/log/server-prep/centos-audit.log
