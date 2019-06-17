627e8315733b720a971d41f5dbdc51ba678e8d3c
shawngmc <shawngmc@gmail.com>
2019-06-17 00:25:10 -0400

    Update antivirus.sh

--- centos-cis/antivirus.sh
+++ centos-cis/antivirus.sh
@@ -10,10 +10,53 @@ setsebool -P antivirus_can_scan_system 1
 # Create a ClamAV Config
 cp /usr/share/clamav/template/clamd.conf /etc/clamd.d/clamd.conf
 sed -i '/^Example/d' /etc/clamd.d/clamd.conf
+sed -i "s/^User <USER>$/User clamscan/gm" /etc/clamd.d/clamd.conf
+sed -i "s/^#LocalSocket/LocalSocket/gm" /etc/clamd.d/clamd.conf
 
+# Enable Freshclam DB Updater
+cp /etc/freshclam.conf /etc/freshclam.conf.bak
+sed -i '/^Example/d' /etc/freshclam.conf
 
+# Add a systemd service for freshclam
+cat > /usr/lib/systemd/system/clam-freshclam.service << "EOF"
+# Run the freshclam as daemon
+[Unit]
+Description = freshclam scanner
+After = network.target
 
-sed -i 's//d' /etc/clamd.d/clamd.conf
-sed -i '/^Example/d' /etc/clamd.d/clamd.conf
-User clamscan
-LocalSocket /var/run/clamd.<SERVICE>/clamd.sock
+[Service]
+Type = forking
+ExecStart = /usr/bin/freshclam -d -c 4
+Restart = on-failure
+PrivateTmp = true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start freshclam
+systemctl enable clam-freshclam.service
+systemctl start clam-freshclam.service
+
+# Clean up the clamav service definition
+mv /usr/lib/systemd/system/clamd@.service /usr/lib/systemd/system/clamd.service
+sed -i "s/clamd@.service/clamd.service/gm" /usr/lib/systemd/system/clamd@scan.service
+cat > /usr/lib/systemd/system/clamd.service << "EOF"
+[Unit]
+Description = clamd scanner daemon
+After = syslog.target nss-lookup.target network.target
+
+[Service]
+Type = simple
+ExecStart = /usr/sbin/clamd -c /etc/clamd.d/clamd.conf --foreground=yes
+Restart = on-failure
+PrivateTmp = true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable clamd.service
+systemctl enable clamd@scan.service
+systemctl start clamd.service
+systemctl start clamd@scan.service
