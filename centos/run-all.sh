8c484015736c773751336ab1592566f1d0bc9020
shawngmc <shawngmc@gmail.com>
2019-06-19 12:10:19 -0400

    adding ec2 artifacts; fixing lynis typo

--- centos/run-all.sh
+++ centos/run-all.sh
@@ -9,17 +9,17 @@ yum check-update 2>&1 | tee /var/log/server-prep/updates.log
 
 # Run customized miitgations
 $scriptDir/antivirus.sh 2>&1 | tee /var/log/server-prep/antivirus.log
-$scriptDir/filesystems.sh 2>&1 | tee /var/log/server-prep/filesystems.log
-$scriptDir/nettime.sh 2>&1 | tee /var/log/server-prep/nettime.log
-$scriptDir/ip.sh 2>&1 | tee /var/log/server-prep/ip.log
 $scriptDir/crontab.sh 2>&1 | tee /var/log/server-prep/crontab.log
-$scriptDir/ssh.sh 2>&1 | tee /var/log/server-prep/ssh.log
+$scriptDir/filesystems.sh 2>&1 | tee /var/log/server-prep/filesystems.log
+$scriptDir/ip.sh 2>&1 | tee /var/log/server-prep/ip.log
+$scriptDir/nettime.sh 2>&1 | tee /var/log/server-prep/nettime.log
 $scriptDir/password.sh 2>&1 | tee /var/log/server-prep/password.log
 $scriptDir/process-hardening.sh 2 >&1 | tee /var/log/server-prep/process-hardening.sh
+$scriptDir/ssh.sh 2>&1 | tee /var/log/server-prep/ssh.log
 
 # Pull scanner and run
 $scriptDir/pull-cis-scanner.sh | tee /var/log/server-prep/centos-audit.log
 $scriptDir/cis-audit.sh 2>&1 | tee /var/log/server-prep/centos-audit.log
 
 $scriptDir/pull-lynis.sh 2>&1 | tee /var/log/server-prep/centos-audit.log
-$scriptDir/lynis/lynix audit system 2>&1 | tee /var/log/server-prep/centos-audit.log
+$scriptDir/lynis/lynis audit system 2>&1 | tee /var/log/server-prep/centos-audit.log
